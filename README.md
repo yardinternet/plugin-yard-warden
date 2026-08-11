b9de44853fdd9d71fa7b86a9e29a4f5dd6766dd6
Roric van Schalkwijk <roric.vanschalkwijk@yard.nl>
2026-08-11 15:02:18 +0200

    release: 1.0.3

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Requires at least: 6.3
 Tested up to: 7.0
 Requires PHP: 7.4
-Stable tag: 1.0.2
+Stable tag: 1.0.3
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
--- yard-warden.php
+++ yard-warden.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  *
  * Plugin Name: Yard | Warden
  * Description: Enhanced password and login security for WordPress.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Yard | Digital Agency
  * Author URI: https://www.yard.nl
  * License: GPLv2 or later
@@ -29,7 +29,7 @@ if (! defined('ABSPATH')) {
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-define('YARD_WARDEN_VERSION', '1.0.2');
+define('YARD_WARDEN_VERSION', '1.0.3');
 define('YARD_WARDEN_REQUIRED_WP_VERSION', '6.3');
 define('YARD_WARDEN_PLUGIN_FILE', __FILE__);
 define('YARD_WARDEN_PLUGIN_DIR_PATH', plugin_dir_path(YARD_WARDEN_PLUGIN_FILE));
